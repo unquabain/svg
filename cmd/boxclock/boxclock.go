b2842c95ebe2c617c5079aa0b69729f22a21ecd5
Ben C. Forsberg <ben.forsberg@mobiletechinc.com>
2022-08-30 15:24:12 -0700

    Split out style and script examples

--- cmd/boxclock/boxclock.go
+++ cmd/boxclock/boxclock.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"image/color"
 
 	"github.com/Unquabain/svg"
 )
+
+//go:embed style.css
+var style string
+
+//go:embed script.js
+var script string
 
 var ColorLight = color.Gray{0xEE}
 var ColorDark = color.Gray{0x44}
@@ -59,50 +66,11 @@ func drawBlocks() svg.G {
 }
 
 func drawStyle() svg.Style {
-	return svg.Style{
-		Content: fmt.Sprintf(`
-text {
-	font-family: sans-serif;
-	font-size: 60px;
-	fill: %s;
-	text-anchor: middle;
-	dominant-baseline: middle;
-}`, svg.FromColor(ColorLight))}
+	return svg.Style{Content: fmt.Sprintf(style, svg.FromColor(ColorLight))}
 }
 
 func drawScript() svg.Script {
-	return svg.Script{Content: `
-function display(unit, multiplier, max, value) {
-	const idPrefix = '' + unit + '-' + multiplier + '-'
-	for (var i = max; i > 0; i -= multiplier) {
-			const id = idPrefix + i
-			if (value >= i) {
-				document.getElementById(id).style.display = 'block'
-			} else {
-				document.getElementById(id).style.display = 'none'
-			}
-	}
-}
-setInterval(function() {
-	const now = new Date()
-	const t = Math.floor(now.getTime() / 1000 - now.getTimezoneOffset()*60)
-	const s = t % 60
-	const m = Math.floor(t / 60) % 60 
-	const h = Math.floor(t / 3600) % 24
-
-	display('s', 1, 4, s % 5)
-	display('s', 5, 10, s % 15)
-	display('s', 15, 45, s % 60)
-
-	display('m', 1, 4, m % 5)
-	display('m', 5, 10, m % 15)
-	display('m', 15, 45, m % 60)
-
-	display('h', 1, 2, h % 3)
-	display('h', 3, 9, h % 12)
-	display('h', 12, 12, h % 24)
-}, 500)
-`}
+	return svg.Script{Content: script}
 }
 
 func main() {
