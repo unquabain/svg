e9bcbca243e0084ffa9494406b338c46ee10e3c6
Ben C. Forsberg <ben.forsberg@mobiletechinc.com>
2022-08-30 18:04:09 -0700

    Removed a debugging line

--- cmd/boxclock/script.js
+++ cmd/boxclock/script.js
@@ -2,6 +2,5 @@ const go = new globalThis.Go()
 WebAssembly.instantiate(Uint8Array.from([%v]),go.importObject)
   .then(result => {
     go.run(result.instance)
-    console.log('setting up tick')
     setInterval(tick, 500)
   })
